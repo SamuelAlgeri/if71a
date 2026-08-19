ea7601b0f3ef0bf49f7d0ca25dc99147e42bce2a
SamuelAlgeri <samuelalgeri@alunos.utfpr.edu.br>
2026-08-19 09:16:25 -0300

    hello2 att 2

--- aula_1/hello2.c
+++ aula_1/hello2.c
@@ -6,8 +6,12 @@ int main()
     {
         printf("Hello World!!!\neh os guri");
         printf("Hello World!!!\neh os guri do %s \n", "gremio");
-        printf("uma canoa se faz com %d paus\n", paus);
-        printf("%d paus são necessários pra fazer uma canoa só \n", paus);
+        printf("uma canoa se faz com %d paus\n", paus+5);
+        printf("%d paus são necessários pra fazer uma canoa só \n", paus-5);
+        printf("%2f paus são necessários pra fazer uma canoa só \n", 20.00/7);
+        printf("O valor eh %c \n", 'a');
+        printf("O valor eh %c \n", 'a'+18);
+        printf("os valores então são:\ninteiro: %d\ncaracter: %c\nfloat: %f\n", paus+28, 'a'+18, 20.00/7);
 
         printf("\n\n\n");
         return 0;
