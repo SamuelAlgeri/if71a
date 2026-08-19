91936f3c89b82a6fefcd9c8e3fdc6d5c2e127a30
SamuelAlgeri <samuelalgeri@alunos.utfpr.edu.br>
2026-08-19 08:57:23 -0300

    hello2

---
+++ aula_1/hello2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int paus = 18;
+
+int main()
+    {
+        printf("Hello World!!!\neh os guri");
+        printf("Hello World!!!\neh os guri do %s \n", "gremio");
+        printf("uma canoa se faz com %d paus\n", paus);
+        printf("%d paus são necessários pra fazer uma canoa só \n", paus);
+
+        printf("\n\n\n");
+        return 0;
+    }
